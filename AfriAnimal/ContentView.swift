e550ac9cad874792a3412a5fcd6e7f827581e61a
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-06-21 09:01:44 +0700

    Initial Commit

---
+++ AfriAnimal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AfriAnimal
+//
+//  Created by Uwais Alqadri on 21/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
